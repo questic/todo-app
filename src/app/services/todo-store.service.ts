f5c827bb1a4dc1e7585d0e19c186dc2aca25c838
Anatoly <31730382+questic@users.noreply.github.com>
2018-04-05 23:40:14 +0300

    Update todo-store.service.ts

--- src/app/services/todo-store.service.ts
+++ src/app/services/todo-store.service.ts
@@ -5,7 +5,6 @@ import {TodoItem} from "../model/todo-item.model";
 export class TodoStoreService {
 
     items: TodoItem[] = [];
-    archive: TodoItem[] = [];
 
     addItem(text: string) {
         let todoItem = new TodoItem(text);
@@ -17,15 +16,10 @@ export class TodoStoreService {
         if (index > -1) {
             let removed = this.items.splice(index, 1)[0];
             removed.completed = true;
-            this.archive.push(removed);
         }
     }
 
     get isEmpty(){
         return this.items.length === 0;
-    }
-
-    get isLazy(){
-        return this.archive.length === 0;
     }
 }
